79cc87be37da748bda4bd0dc03ffbada1e5a0c03
杨思远 <1017599853@qq.com>
2022-06-23 18:41:56 +0800

    first commit

---
+++ api-passenger/src/main/java/org/mashibing/apipassenger/ApiPassengerApplication.java
@@ -0,0 +1,20 @@
+package org.mashibing.apipassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 杨思远
+ * @version 1.0
+ * @title
+ * @date 2022/6/23 16:48
+ * @description
+ */
+@SpringBootApplication
+public class ApiPassengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassengerApplication.class);
+    }
+
+}
---
+++ api-passenger/src/main/java/org/mashibing/apipassenger/controller/TestController.java
@@ -0,0 +1,22 @@
+package org.mashibing.apipassenger.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 杨思远
+ * @version 1.0
+ * @title
+ * @date 2022/6/23 16:53
+ * @description
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+
+        return "test api passenger";
+    }
+
+}
