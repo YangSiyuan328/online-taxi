56799b59c8c67373aa33222bb296b9cea322ca65
杨思远 <1017599853@qq.com>
2022-06-24 00:43:05 +0800

    注册中心搭建&测试

--- api-passenger/src/main/java/org/mashibing/apipassenger/ApiPassengerApplication.java
+++ api-passenger/src/main/java/org/mashibing/apipassenger/ApiPassengerApplication.java
@@ -2,6 +2,7 @@ package org.mashibing.apipassenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author 杨思远
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiPassengerApplication {
 
     public static void main(String[] args) {
