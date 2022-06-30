03052553b753d8e6f53f62cc01741189b92c3991
杨思远 <1017599853@qq.com>
2022-06-30 13:45:03 +0800

    jwt拦截器的编写

---
+++ api-passenger/src/main/java/org/mashibing/apipassenger/interceptor/JwtInterceptor.java
@@ -0,0 +1,55 @@
+package org.mashibing.apipassenger.interceptor;
+
+import com.alibaba.fastjson.JSONObject;
+import com.auth0.jwt.exceptions.AlgorithmMismatchException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.util.JwtUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+/**
+ * @author 杨思远
+ * @version 1.0
+ * @title
+ * @date 2022/6/30 11:35
+ * @description
+ */
+public class JwtInterceptor implements HandlerInterceptor {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        boolean result = true;
+        String resultString = "";
+
+        String token = request.getHeader("Authorization");
+
+        try {
+            JwtUtils.parseToken(token);
+        } catch (SignatureVerificationException e) {
+            resultString = "token sign error";
+            result = false;
+        } catch (TokenExpiredException e) {
+            resultString = "token time out";
+            result = false;
+        } catch (AlgorithmMismatchException e) {
+            resultString = "token algorithm mismatch";
+            result = false;
+        } catch (Exception e) {
+            resultString = "token invalid";
+            result = false;
+        }
+        if (!result) {
+            PrintWriter out = response.getWriter();
+            out.print(JSONObject.toJSON(ResponseResult.fail(resultString).toString()));
+        }
+
+        return result;
+    }
+}
