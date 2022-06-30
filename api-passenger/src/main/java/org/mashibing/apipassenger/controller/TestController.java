65012d1cb008a27778a4358d592a0c76ca51a8ff
杨思远 <1017599853@qq.com>
2022-06-30 11:33:27 +0800

    token测试controller的准备

--- api-passenger/src/main/java/org/mashibing/apipassenger/controller/TestController.java
+++ api-passenger/src/main/java/org/mashibing/apipassenger/controller/TestController.java
@@ -1,5 +1,6 @@
 package org.mashibing.apipassenger.controller;
 
+import com.mashibing.internalcommon.dto.ResponseResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,24 @@ public class TestController {
     public String test(){
 
         return "test api passenger";
+    }
+
+    /**
+     * 需要有 token
+     * @return
+     */
+    @GetMapping("/authTest")
+    public ResponseResult authTest(){
+        return ResponseResult.success("auth test");
+    }
+
+    /**
+     * 不需要有 token
+     * @return
+     */
+    @GetMapping("/noAuthTest")
+    public ResponseResult noAuthTest(){
+        return ResponseResult.success("no auth test");
     }
 
 }
